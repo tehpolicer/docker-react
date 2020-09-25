6155d8ccfe843aa9ce70223c945696c5f11375a9
Dilayn Trayanov <d.trayanov89@gmail.com>
2020-09-25 10:57:06 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Something here
         </a>
       </header>
     </div>
